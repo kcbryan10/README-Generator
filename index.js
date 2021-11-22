fbc0669e66c8a49cbbc0e8ae31c48123d99c4bf6
kcbryan <kcbryan10@gmail.com>
2021-11-21 19:06:48 -0600

    generateMarkdown function complete created

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 // TODO: Include packages needed for this application
-const { fstat } = require("fs");
+const fs = require("fs");
 const inquirer = require("inquirer");
+const generateMarkdown = require("./Develop/utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 const questions = [
+
+    {
+        type: "checkbox",
+        name: "License",
+        message: "Choose a licenese for the project:",
+        choices: ["MIT", "Apache", "GNU", "None"],
+        validate: LicInput => {
+            if (LicInput) {
+                return true;
+            }
+            else {
+                console.log("please choose a licenese");
+                return false;
+            }
+        }
+    },
+
     {
         type: "input",
         name: "Title",
-        message: "whats the title of the project?",
+        message: "whats the title of the project?:",
         validate: TitleInput => {
             if (TitleInput) {
                 return true;
@@ -22,7 +40,7 @@ const questions = [
     {
         type: "input",
         name: "Description",
-        message: "Give a description for the project",
+        message: "Give a description for the project:",
         validate: DescriptionInput => {
             if (DescriptionInput) {
                 return true;
@@ -37,9 +55,9 @@ const questions = [
     {
         type: "input",
         name: "Installation",
-        message: "How should you install your application?",
-        validate: InstalInput => {
-            if (InstalInput) {
+        message: "How should you install your application?:",
+        validate: InstallInput => {
+            if (InstallInput) {
                 return true;
             }
             else {
@@ -52,7 +70,7 @@ const questions = [
     {
         type: "input",
         name: "Usage",
-        message: "How do you use this application?",
+        message: "How do you use this application?:",
         validate: UsageInput => {
             if (UsageInput) {
                 return true;
@@ -66,7 +84,7 @@ const questions = [
     {
         type: "input",
         name: "Contribution",
-        message: "Contributions for the project?",
+        message: "Contributions for the project?:",
         validate: ContInput => {
             if (ContInput) {
                 return true;
@@ -79,25 +97,9 @@ const questions = [
     },
 
     {
-        type: "checkbox",
-        name: "License",
-        message: "Choose a licenese for the project",
-        Choices: [1,2,3,4],
-        validate: LicInput => {
-            if (LicInput) {
-                return true;
-            }
-            else {
-                console.log("please enter a title");
-                return false;
-            }
-        }
-    },
-
-    {
         type: "input",
         name: "Test",
-        message: "how do you test this application?",
+        message: "how do you test this application?:",
         validate: TestInput => {
             if (TestInput) {
                 return true;
@@ -112,7 +114,7 @@ const questions = [
     {
         type: "input",
         name: "Questions",
-        message: "",
+        message: "Any common questions?:",
         validate: QuestionInput => {
             if (QuestionInput) {
                 return true;
@@ -122,10 +124,12 @@ const questions = [
                 return false;
             }
         }
-    }
+    },
+
 ];
 
 // TODO: Create a function to write README file
+
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) => {
         if(err)
@@ -135,13 +139,15 @@ function writeToFile(fileName, data) {
 };
 
 // TODO: Create a function to initialize app
+
 function init() {
     inquirer.prompt(questions)
     .then (function (userInput){
         console.log(userInput)
-        writeToFile("README.md", genReadme(userInput));
+        writeToFile("README.md", generateMarkdown(userInput));
     });
 };
 
 // Function call to initialize app
+
 init();
